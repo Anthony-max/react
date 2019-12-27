7b0e8b27d0f075cf6024e3dee57f1c1c9893ed00
anthony Obregon <anthony_cai_1997@outlook.com>
2019-12-27 00:33:59 -0300

    se agrego instruccion fetch

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -3,8 +3,9 @@ import Location from './Location';
 import WeatherData from './WeatherData/';
 import "./styles.css"
 import {
-    SNOW,
-    WINDY,
+    
+   
+    CLOUD,
 } from "./../../constants/weather";
 
 const location = "Buenos Aires,ar"
@@ -14,17 +15,17 @@ const url_base_weather = "https://api.openweathermap.org/data/2.5/weather";
 const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
 const data = {
     temperature: 5,
-    weatherState: SNOW,
+    weatherState: CLOUD,
     humidity: 10,
     wind:'10 m/s',
 }
 
-const data2 = {
+/*const data2 = {
     temperature: 5,
     weatherState: WINDY,
     humidity: 10,
     wind:'10 m/s',
-}
+}*/
 class WeatherLocation extends Component {
 
     constructor()
@@ -35,24 +36,48 @@ class WeatherLocation extends Component {
             data: data,
         };
     }
+
+    getWeatherState = weather_data => {
+        return CLOUD;
+    }
+    getData = weather_data => {
+        const {humidity, temp} = weather_data.main;
+        const {speed} = weather_data.wind;
+        const weatherState = this.getWeatherState();
+
+        const data = {
+            humidity,
+            temperature: temp,
+            weatherState,
+            wind: `${speed} m/s`,
+        }
+
+        return data;
+    }
+
     handleUpdateClick = () => 
     {
         fetch(api_weather).then(resolve => {
             
             return resolve.json();
         }).then(data=>{
-            console.log(data);
+            const newWeather = this.getData(data);
+            console.log(newWeather);
             debugger;
-
+            
+            this.setState({
+                data: newWeather,
+            });
+            
         });
-
+        /*
         console.log("actualizado");
         
         this.setState({
             city: "Buenos Aires!!",
             data:data2
             //usar setState para modificar
-        });
+        });*/
     }
     render()
     {
